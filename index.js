ca6850f193ab0e7836d5344ea54044cc5aa13e5a
elizatinu <75367989+elizatinu@users.noreply.github.com>
2021-02-24 14:34:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("hello world")
+})
+
+app.get('/login', (req,res)=>{
+    res.send("login page")
+})
+
+app.listen(8000);
+
+console.log("hello world");
